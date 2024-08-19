84da75edc92739789ef0337d9e0106de02fcc06d
Gregori Schultz <gregorischultz07@gmail.com>
2024-08-19 18:06:56 +0200

    home feita falta mudar a cor do nome

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,8 @@
 import React from "react";
 import Header from "../componentes/Header"; 
 import '../estilos/Home.css';
-import '../componentes/Footer.jsx';
+import SocialMedia from '../componentes/SocialMedia';
+
 
 
 const Home = () => {
@@ -10,16 +11,18 @@ const Home = () => {
     return (
   <><Header />
   <div className='all-home'>
-        <div className="home">
-          <section className="home-section home-text-animation">
-            <p className="home-name">GREGORI SCHULTZ</p>
-            <div className="home-subtitle">
-              <p className="home-subtitle-1">Développeur Web</p><span>|</span>
-              <p className="home-subtitle-2">Étudiant Concepteur Développeur d’Application</p>
-            </div>
-          </section> 
-         </div>
+      <div className="home">
+        <section className="home-section home-text-animation">
+          <p className="home-name-1">GREGORI</p>
+          <p className="home-name-2">SCHULTZ</p>
+          <div className="home-subtitle">
+            <p className="home-subtitle-1">Développeur Web</p><span>|</span>
+            <p className="home-subtitle-2">Étudiant Concepteur Développeur d’Application</p>
+          </div>
+        </section>
+       <SocialMedia/>
       </div>
+    </div>
       
       </>
     )
