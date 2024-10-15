512a19c1d672947abb12316cedc4da1a38f52235
Gregori Schultz <gregorischultz07@gmail.com>
2024-10-15 15:18:07 +0200

    add wcs

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
           <section className="about-text">
               <p className="P0">Bonjour, je m'appelle <span> Gregori Schultz.</span> </p>
               
-              <p className="P">Je suis à <span>la recherche d'une alternance dans le cadre de la formation Concépteur Développeur d'Applications proposée par le CEFIM.</span> J'aimerais ensuite gagner de l'expérience entant que développeur Back-end, pour un jour exercer entant que développeur Full-Stack.</p>
+              <p className="P">Je suis à <span>la recherche d'une alternance dans le cadre de la formation Concépteur Développeur d'Applications proposée par la Wild Code School.</span> J'aimerais ensuite gagner de l'expérience entant que développeur Back-end, pour un jour exercer entant que développeur Full-Stack.</p>
                             
               <p className="P">Je suis <span>développeur Front-ent de formation,</span> je maîtrise particulièrement les languages suivants : <span>JavaScript</span>, <span>ReactJS</span> et <span>HTML/CSS.</span> Je suis également familier avec les outils essentiels : <span>NodeJS, SQL e MySQL, ExpressJS.</span> J'étudie actuellement de manière autodidacte le <span>Python et TypeScript,</span> afin d'enrichir mes connaissances et gagner en compétences.</p>
               
